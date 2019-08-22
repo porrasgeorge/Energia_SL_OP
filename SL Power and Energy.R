5f83862c7e3639b1cfd741e43a1a52ade2cccb05
jporras <porrasgeorge@gmail.com>
2019-08-22 09:53:49 -0600

    2 periodos por tipo

--- SL Power and Energy.R
+++ SL Power and Energy.R
@@ -1,10 +1,11 @@
 library(ggplot2)
 library(dplyr)
 library(lubridate)
+library(openxlsx)
 
-corte <- 8400
+corte <- 8350
 
-SL = read.csv("SL_Potencias.CSV", stringsAsFactors = F)
+SL = read.csv("SL_Potencias2.CSV", stringsAsFactors = F)
 names(SL)[1] = 'Hora'
 names(SL)[2] = 'U1'
 names(SL)[3] = 'U2'
@@ -22,12 +23,12 @@ periodo <- function(fecha_hora){
                                       2, 
                                       3),
                                ifelse(hora < 1730, 
-                                      2,
+                                      4,
                                       ifelse(hora < 2000,
                                              ifelse(wday(fecha_hora) %in% c(1, 7),
-                                                    2, 
-                                                    3),
-                                             1)
+                                                    4, 
+                                                    5),
+                                             6)
                                       )
                               )
                         )
@@ -46,7 +47,8 @@ SL <- SL %>% arrange(Hora) %>%
   group_by(anho, mes, dia, hora, minuto) %>%
   summarise(U1 = sum(U1)/15, U2 = sum(U2)/15, periodo = mean(periodo))
 
-SL$periodo <- factor(SL$periodo, levels = c(1, 2, 3), labels = c("Noche", "Valle", "Punta"))
+SL$periodo <- factor(SL$periodo, levels = c(1, 2, 3, 4, 5, 6), 
+                     labels = c("Noche1", "Valle1", "Punta1", "Valle2", "Punta2", "Noche2"))
 
 
 SL <- SL %>% mutate(E1 = ifelse(U1 > corte, 
@@ -54,6 +56,16 @@ SL <- SL %>% mutate(E1 = ifelse(U1 > corte,
                                 0), 
                     E2 = ifelse(U2 > corte, 
                                 (U2 - corte) / 4, 
-                                0)) %>%
+                                0))
+
+
+SL_dia <- SL %>% 
   group_by(anho, mes, dia, periodo) %>%
-  summarise(T1 = sum(E1), T2 = sum(E2))
+  summarise(Energia_U1 = sum(E1), Energia_U2 = sum(E2))
+
+SL_mes <- SL %>% 
+  group_by(anho, mes, periodo) %>%
+  summarise(Energia_U1 = sum(E1), Energia_U2 = sum(E2))
+
+DS_list <- list("Diario" = SL_dia, "Mensual" = SL_mes)
+write.xlsx(DS_list, file = "Energia por periodo.xlsx")
