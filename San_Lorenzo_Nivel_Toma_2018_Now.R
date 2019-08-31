056d424070aa99023a01a1bbfd914011d14e6362
Jorge Porras Araya <porrasgeorge@gmail.com>
2019-08-30 23:14:55 -0600

    actualizacion archivo SL data

--- San_Lorenzo_Nivel_Toma_2018_Now.R
+++ San_Lorenzo_Nivel_Toma_2018_Now.R
@@ -1,9 +1,13 @@
 library(tidyverse)
+library(openxlsx)
+library(lubridate)
+
+
 SL_Toma <- read.csv("San Lorenzo Nivel Toma.txt", stringsAsFactors = F)
 SL_Toma$TIME <- as.POSIXct(SL_Toma$TIME)
 
-glimpse(SL_Toma)
-
+# los datos vienen de 3 diferentes columnas de la tabla en la Base de datos,
+# debido al cambio de nombre en la variable de Survalent
 SL_Toma01 <- SL_Toma %>% 
   filter(!is.na(Nivel01)) %>%
   mutate(Hora = TIME,
@@ -24,28 +28,25 @@ SL_Toma03 <- SL_Toma %>%
          Nivel = Nivel03,
          Flag = Flag03) %>%
   select(Hora, Nivel, Flag)
-#rm(SL_Toma)
 
 NivelToma <- rbind(SL_Toma01, SL_Toma02, SL_Toma03)
 rm(SL_Toma, SL_Toma01, SL_Toma02, SL_Toma03)
 
-NivelTomaTelemFailed <- NivelToma %>% filter(Flag != 1)
-NivelTomaTelemFailed <- cbind(NivelTomaTelemFailed, rep(0, nrow(NivelTomaTelemFailed)))
-names(NivelTomaTelemFailed)[4] = "Rank"
-
-
-n = 1
-for (i in 1:(nrow(NivelTomaTelemFailed)- 1)) {
-  NivelTomaTelemFailed$Rank[i] = n
-  if ((NivelTomaTelemFailed$Hora[i] + 300) != NivelTomaTelemFailed$Hora[i+1]){
-    n = n+1
-  }
-}
-
-
-
-
 # si se desea eliminar los telemetry failed del punto anterior
+# 
+# NivelTomaTelemFailed <- NivelToma %>% filter(Flag != 1)
+# NivelTomaTelemFailed <- cbind(NivelTomaTelemFailed, rep(0, nrow(NivelTomaTelemFailed)))
+# names(NivelTomaTelemFailed)[4] = "Rank"
+# 
+# 
+# n = 1
+# for (i in 1:(nrow(NivelTomaTelemFailed)- 1)) {
+#   NivelTomaTelemFailed$Rank[i] = n
+#   if ((NivelTomaTelemFailed$Hora[i] + 300) != NivelTomaTelemFailed$Hora[i+1]){
+#     n = n+1
+#   }
+# }
+# 
 # TelemFailedRankingGroup <- NivelTomaTelemFailed %>% 
 #   group_by(Rank) %>% 
 #   summarise(Hora = min(Hora), minutos = 5*n()) %>% 
@@ -56,3 +57,22 @@ for (i in 1:(nrow(NivelTomaTelemFailed)- 1)) {
 # NivelToma %>% filter(Flag != 1)
 # 
 
+NivelToma$NivelSobreCresta <- ifelse(NivelToma$Nivel>0, NivelToma$Nivel/100, 0)
+NivelToma$Caudal <- 94.047 * sqrt(NivelToma$NivelSobreCresta ^ 3)
+NivelToma$VolumenVertido <- 300* NivelToma$Caudal
+
+
+NivelToma <- NivelToma %>% arrange(Hora) %>% 
+  mutate(anho = year(Hora), 
+         mes = month(Hora),
+         dia = day(Hora),
+         hora = hour(Hora)) %>%
+  group_by(anho, mes, dia, hora) %>%
+  summarise(CaudalAVG = mean(Caudal),
+            VolumenVertidoT = sum(VolumenVertido))
+
+
+
+
+DS_list <- list("Nivel" = NivelToma)
+write.xlsx(DS_list, file = "C:/Data Science/ArhivosGenerados/Nivel y Caudal San Lorenzo.xlsx")
